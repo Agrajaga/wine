abf53005f97b2a8b82a674f8d014de117a433bd1
Michael <soplomiha@gmail.com>
2022-03-28 15:24:30 +0300

    Add wine loading from excel

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-
+from pandas import read_excel
 
 def get_winery_age():
     measurements = ('лет', 'год', 'года', 'года', 'года', 'лет', 'лет', 'лет', 'лет', 'лет')
@@ -18,8 +18,11 @@ env = Environment(
 
 template = env.get_template('template.html')
 
+excel_wines_filename = 'wine.xlsx'
+
 rendered_page = template.render(
     winery_age=get_winery_age(),
+    wines=read_excel(excel_wines_filename).to_dict(orient='records')
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
