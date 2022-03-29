09fed0d60b8210be5b6390879965ab401ac8bdda
Michael <soplomiha@gmail.com>
2022-03-29 13:47:06 +0300

    Fix naming for universal purposes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from pandas import read_excel
 
 
-def get_winery_age(start_year):
+def get_manufacture_age(start_year):
     age = str(datetime.date.today().year - start_year)
     
     age_tens = '0' if len(age) <= 1 else age[-2]
@@ -21,13 +21,13 @@ def get_winery_age(start_year):
     return f'{age} {measure}'
 
 
-def get_drinks_by_category(filename):
-    drinks_by_category = collections.defaultdict(list)
-    file_content = read_excel(
-        filename, keep_default_na=False).to_dict(orient='records')
-    for element in file_content:
-        drinks_by_category[element['Категория']].append(element)
-    return drinks_by_category
+def get_products_by_category(filepath):
+    products_by_category = collections.defaultdict(list)
+    production = read_excel(
+        filepath, keep_default_na=False).to_dict(orient='records')
+    for product in production:
+        products_by_category[product['Категория']].append(product)
+    return products_by_category
 
 
 if __name__ == "__main__":
@@ -40,8 +40,8 @@ if __name__ == "__main__":
     template = env.get_template('template.html')
 
     rendered_page = template.render(
-        winery_age=get_winery_age(start_year),
-        drinks_by_category=get_drinks_by_category('wine.xlsx')
+        winery_age=get_manufacture_age(start_year),
+        drinks_by_category=get_products_by_category('wine.xlsx')
     )
 
     with open('index.html', 'w', encoding="utf8") as file:
